c1907ba7ee6f713c327e4a00ffde7c2026bde9db
Luis Hernandez <lhdez@iuma.ulpgc.es>
2020-03-29 08:24:01 +0100

    cambios menores

--- app/src/main/java/es/ulpgc/eite/cleancode/clickcounter/counter/CounterRouter.java
+++ app/src/main/java/es/ulpgc/eite/cleancode/clickcounter/counter/CounterRouter.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import es.ulpgc.eite.cleancode.clickcounter.app.AppMediator;
 import es.ulpgc.eite.cleancode.clickcounter.app.ClicksToCounterState;
 import es.ulpgc.eite.cleancode.clickcounter.app.CounterToClicksState;
+import es.ulpgc.eite.cleancode.clickcounter.clicks.ClicksActivity;
 
 public class CounterRouter implements CounterContract.Router {
 
@@ -20,7 +21,7 @@ public class CounterRouter implements CounterContract.Router {
   @Override
   public void navigateToNextScreen() {
     Context context = mediator.getApplicationContext();
-    Intent intent = new Intent(context, CounterActivity.class);
+    Intent intent = new Intent(context, ClicksActivity.class);
     intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
     context.startActivity(intent);
   }
